58fd28bf0393540d5ea872f59b83d276d8bfa5fc
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2022-10-06 22:22:32 -0300

    Create setTimeout.js and README.md

---
+++ set-timeout/README.md
@@ -0,0 +1,1 @@
+<h1>🚧Em Desenvolvimento🚧</h1>
---
+++ set-timeout/setTimeout.js
@@ -0,0 +1,6 @@
+// setTimeout serve para executar uma função depois de um determinado
+// tempo que sera determinado em milissegundos.
+
+setTimeout(function() {
+  console.log("Função executada");
+},  1500)
