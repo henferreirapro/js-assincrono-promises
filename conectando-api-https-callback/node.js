1eb0455749a79f2dc99ed54978205671774c2b0e
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2022-10-08 15:40:30 -0300

    Connecting API, HTTPS and Callbacks

---
+++ conectando-api-https-callback/node.js
@@ -0,0 +1,17 @@
+const https = require('https');
+const API = 'https://jsonplaceholder.typicode.com/users?_limit=2';
+
+https.get(API, res => {
+  console.log(res.statusCode)
+  // 200
+})
+
+console.log("Conectando API")
+
+// Resposta Correta:
+  // Conectando API
+  // 200
+
+// Quando a linha 4 é executada o JS entende que é uma Callback 
+// então continua lendo as linhas até o final e assim que terminar
+// ele volta na linha 4 caso finalizado ele executa o resultado
